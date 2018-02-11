5fe59326ecd8926f65ce974776cd3bcf19b7baa7
alex <alex@DESKTOP-J26H4GL>
2018-02-10 18:54:06 -0700

    Added the class that will house the main method
    Added an assignment class to house the main method and be the
    implementation point of my classes and methods

---
+++ src/main/java/com/akirix/newHire/Assignment.java
@@ -0,0 +1,20 @@
+package com.akirix.newHire;
+
+public class Assignment {
+
+	public static void main(String[] args) {
+		
+		//Run the first assignent demo followed by the second assignment demo
+		runFirstAssignment();
+		runSecondAssignment();
+	}
+	
+	private static void runFirstAssignment() {
+		
+	}
+	
+	private static void runSecondAssignment() {
+		
+	}
+
+}
