385266a6b4442e72b8a65443e0ae4a2ac148b019
alex <alex@DESKTOP-J26H4GL>
2018-02-10 17:19:58 -0700

    Got started on the AkirixComparableQuicksorter class
    I originally had the methods as non-static and paramaratized T at the
    class level, since then I thought it through a bit and realized it would
    be easier to implement this class if I did the methods static and
    paramaratized T at the method level.  During implementation I will be
    able to sort any type T without having to make multiple instances of
    AkirixComparableQuicksorter; instead I can just use it statically when
    doing it this way.  Yay!!

---
+++ src/main/java/com/akirix/newHire/AkirixComparableQuicksorter.java
@@ -0,0 +1,35 @@
+package com.akirix.newHire;
+
+public class AkirixComparableQuicksorter {
+
+	public static <T extends Comparable<T>> void quickSort(T[] a, int leftIndex, int rightIndex) {
+		if(leftIndex <= rightIndex) return; //Stop when left and right indexes meet
+		int j = partition(a, leftIndex, rightIndex); //Partition current
+		quickSort(a, leftIndex, j - 1); //Sort next index level to the right from the left index
+		quickSort(a, j + 1, rightIndex); //Sort next index level to the left from the the right index
+	}
+	
+	private static <T extends Comparable<T>> int partition(T[] a, int leftIndex, int rightIndex) {
+		int i = leftIndex; //Set i as leftmost
+		int j = rightIndex + 1; //Set j as rightmost
+		T part = a[rightIndex]; //Get comparable at right index of array
+		while(true) { //Continue until break when i less than or equal to j
+			while(less(a[++i], part)) if(i == rightIndex) break; 
+			while(less(part, a[--j])) if (j == leftIndex) break;
+			if(i >= j) break;
+			exch(a, i, j);
+		}
+		exch(a, leftIndex, j);
+		return j;
+	}
+	
+	private static <T extends Comparable<T>> boolean less(T a, T b) {
+		return a.compareTo(b) <= 0;
+	}
+	
+	private static <T extends Comparable<T>> void exch(T[] a, int i, int j) {
+		T b = a[i];
+		a[i] = a[j];
+		a[j] = b;
+	}
+}
